9db44aab6da478831cd883bc21687af004b1c137
Kev <kevinserrano010@gmail.com>
2023-05-26 13:40:10 -0300

    Comprobbar si la solicitud fue exitosa para luego obtener la informacion

--- scrap.py
+++ scrap.py
@@ -5,4 +5,6 @@ import pandas as pd
 def crawl(url):
     # Realizar una solicitud HTTP GET a la URL
     response = requests.get(url)
-    
+    # Comprobar si la solicitud fue exitosa (código de estado 200)
+    if response.status_code == 200:
+        data = requests.get(url)
